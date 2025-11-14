02b884f6b1f32e70e870b5cb80528dd4a45bc0b2
finkzhang <finkzhang@tencent.com>
2025-11-14 21:27:32 +0800

    init(nuklear_demo): overview demo

---
+++ nuklear_demo/src/nuklear.cpp
@@ -0,0 +1,9 @@
+/*
+ * @file: nuklear.cpp
+ * @brief: nuklear implementation
+ */
+
+#define NK_IMPLEMENTATION
+#define NK_SDL_GL3_IMPLEMENTATION
+#include "nuklear_inc.h"
+#include "nuklear/nuklear_sdl_gl3.h"
---
+++ nuklear_demo/src/nuklear_inc.h
@@ -0,0 +1,14 @@
+/* NOTE: Include this header as it unifies the compilation configurations */
+#ifndef NUKLEAR_INC_H_
+#define NUKLEAR_INC_H_
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#include "nuklear/nuklear.h"
+
+#endif // NUKLEAR_INC_H_
