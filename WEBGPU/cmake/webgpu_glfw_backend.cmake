3330c5afcbfdd17ecfb9f7d0bad8adc559f410ea
inkfin <inkfinite@qq.com>
2026-01-10 22:21:43 +0800

    chore: add CMakePresets.json & fix dawn building

--- WEBGPU/cmake/webgpu_glfw_backend.cmake
+++ WEBGPU/cmake/webgpu_glfw_backend.cmake
@@ -1,6 +1,7 @@
 add_library(webgpu_glfw_backend INTERFACE)
 
 if(EMSCRIPTEN)
+
     target_link_options(webgpu_glfw_backend INTERFACE
         --use-port=emdawnwebgpu
         -sUSE_GLFW=3
@@ -9,9 +10,28 @@ if(EMSCRIPTEN)
         # -sEXPORT_ES6=1
         # -sENVIRONMENT=web
     )
-else()
-    add_subdirectory(path/to/dawn)  # 生成 webgpu_dawn/webgpu_glfw/glfw
-    target_link_libraries(webgpu_glfw_backend INTERFACE
-        webgpu_dawn webgpu_glfw glfw
+
+else() # Not EMSCRIPTEN
+
+    set(DAWN_CLONE_DIR "${CMAKE_SOURCE_DIR}/../dawn" CACHE PATH
+        "Path to local Dawn clone, default to ${CMAKE_SOURCE_DIR}/../dawn"
     )
+    set(DAWN_VERSION "HEAD" CACHE STRING
+        "Dawn git tag/commit/branch, default to HEAD"
+    )
+
+    include(FetchContent)
+    FetchContent_Declare(dawn
+        GIT_REPOSITORY https://github.com/google/dawn.git
+        GIT_TAG        ${DAWN_VERSION}
+        GIT_SHALLOW    TRUE
+        GIT_PROGRESS   TRUE
+        SOURCE_DIR     ${DAWN_CLONE_DIR}
+    )
+    FetchContent_MakeAvailable(dawn)
+
+    target_link_libraries(webgpu_glfw_backend INTERFACE
+            webgpu_dawn webgpu_glfw glfw
+        )
+
 endif()
