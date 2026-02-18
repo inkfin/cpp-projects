97e7225832e0fafadc551e82d4611cc7f21a7f09
inkfin <inkfinite@qq.com>
2026-02-18 13:59:17 +0800

    feat(CParser): tl_log.h thread_local buffer

--- CParser/tl_log.h
+++ CParser/tl_log.h
@@ -1,33 +1,37 @@
 /* vim: set ft=c : -*- mode: c -*-
  * tl_log.h
- *   Logging utilities for C projects
+ *   Simple logging utilities for GNU99+ C projects.
  *
- *   Configurations:
+ *   Configuration:
  *
  *     TL_LOG_LEVEL:
- *       DEBUG, INFO, WARN, ERROR
+ *       One of TL_LOG_LEVEL_DEBUG/INFO/WARN/ERROR.
+ *       Lower-severity macros become no-ops at compile time.
  *
  *     TL_LOG_TO_FILE:
- *       Filename that the log will be saved to
+ *       Enables file-backed logging via tl_log_init()/tl_log_deinit().
+ *       When enabled, TL_LOG_STREAM/TL_LOG_STREAM_ERR are overridden.
  *
  *     TL_LOG_STREAM, TL_LOG_STREAM_ERR:
- *       Specifies the stream to log to
- *       `TL_LOG_TO_FILE`, if defined, will overwrite this setting
- *
- *     TL_LOG_NO_STDARG:
- *       Use an simple macro implementation as alternatives
+ *       Output streams for normal and error logs.
  *
  *     TL_LOG_FAST:
- *       Disable expensive runtime log features like timestamp
+ *       Disables timestamp generation.
  *
- *     TL__LOG_IMPL
- *       Log implementation function, args: (FILE*, fmt, ...)
+ *     TL_LOG_MSG_BUF_SIZE:
+ *       Size of reusable per-thread message buffer used for formatted logs.
+ *       If a message is too long, it is truncated and ends with "...".
+ *
+ *   Notes:
+ *     Logger appends '\n' automatically; pass messages without trailing newline.
  */
 #ifndef TINYLIB_LOGS_H
 #define TINYLIB_LOGS_H
 
 
 /** Configurations **/
+
+#include <stdio.h>
 
 #define TL_LOG_LEVEL_DEBUG 0
 #define TL_LOG_LEVEL_INFO  1
@@ -37,18 +41,39 @@
 #  define TL_LOG_LEVEL  TL_LOG_LEVEL_INFO
 #endif
 
+#ifndef TL_LOG_MSG_BUF_SIZE
+#  define TL_LOG_MSG_BUF_SIZE 1024
+#endif
 
-#ifdef TL_LOG_TO_FILE
-#  define TL_LOG_STREAM tl__g_log_file
-#  define TL_LOG_STREAM_ERR tl__g_log_file
-#  include <stdlib.h>
-static FILE* tl__g_log_file = NULL;
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+#  define TL_LOG__THREAD_LOCAL _Thread_local
+#elif defined(__GNUC__) || defined(__clang__)
+#  define TL_LOG__THREAD_LOCAL __thread
+#elif defined(_MSC_VER)
+#  define TL_LOG__THREAD_LOCAL __declspec(thread)
+#else
+#  define TL_LOG__THREAD_LOCAL
+#endif
+
+
+#if defined(TL_LOG_TO_FILE)
+#define TL_LOG_STREAM     tl__g_log_file
+#define TL_LOG_STREAM_ERR tl__g_log_file
+#include <stdlib.h>
+static FILE *tl__g_log_file = NULL;
 static inline void
-tl_log_init(const char* filename) {
+tl_log_init(const char *filename) {
     tl__g_log_file = fopen(filename, "a");
     if (!tl__g_log_file) {
         perror("Failed to open log file");
         exit(EXIT_FAILURE);
+    }
+}
+static inline void
+tl_log_deinit() {
+    if (tl__g_log_file) {
+        fclose(tl__g_log_file);
+        tl__g_log_file = NULL;
     }
 }
 #endif
@@ -56,96 +81,90 @@ tl_log_init(const char* filename) {
 #  define TL_LOG_STREAM stdout
 #endif
 #ifndef TL_LOG_STREAM_ERR
-# define TL_LOG_STREAM_ERR  TL_LOG_STREAM
+#  define TL_LOG_STREAM_ERR stderr
 #endif
 
 
 /** Implementation **/
 
 #define TL_LOG__FMT_STR "%s:%d %s: "
-#if TL_LOG_FAST
-#include <stdio.h>
-#  if TL_LOG_NO_STDARG
-#define TL__LOG_IMPL(stream, fmt, ...)                                                          \
-    do {                                                                                        \
-        fprintf(stream, TL_LOG__FMT_STR fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
-        fflush(stream);                                                                         \
-    } while (0)
-#  else  /* !TL_LOG_NO_STDARG */
-#include <stdarg.h>
-static inline void
-tl__log_impl(FILE *stream,
-             const char *file,
-             int line,
-             const char *func,
-             const char *fmt,
-             ...) {
-    fprintf(stream, TL_LOG__FMT_STR, file, line, func);
 
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stream, fmt, args);
-    va_end(args);
-
-    fprintf(stream, "\n");
-    fflush(stream);
-}
-#define TL__LOG_IMPL(stream, fmt, ...) tl__log_impl(stream, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
-#  endif
-#else  /* !TL_LOG_FAST */
 #include <time.h>
-#include <stdio.h>
-#  if TL_LOG_NO_STDARG
-#define TL__LOG_IMPL(stream, fmt, ...)                                                                      \
-    do {                                                                                                    \
-        time_t     t = time(NULL);                                                                          \
-        struct tm *tm_info = localtime(&t);                                                                 \
-        char       time_buf[20];                                                                            \
-        strftime(time_buf, sizeof time_buf, "%Y-%m-%d %H:%M:%S", tm_info);                                  \
-        fprintf(stream, "[%s] " TL_LOG__FMT_STR fmt "\n", time_buf, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
-        fflush(stream);                                                                                     \
-    } while (0)
-#  else  /* !TL_LOG_NO_STDARG */
-#include <stdarg.h>
-static inline void
-tl__log_impl(FILE *stream,
-             const char *file,
-             int line,
-             const char *func,
-             const char *fmt,
-             ...) {
-    time_t t = time(NULL);
 
-    struct tm tm_info;
-#    if defined(_WIN32)
-    localtime_s(&tm_info, &t);
-#    else
-    localtime_r(&t, &tm_info);
-#    endif
-
-    char time_buf[20];
-    strftime(time_buf, sizeof time_buf, "%Y-%m-%d %H:%H:%S", &tm_info);
-
-    fprintf(stream, "[%s] " TL_LOG__FMT_STR, time_buf, file, line, func);
-
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stream, fmt, args);
-    va_end(args);
-
-    fprintf(stream, "\n");
-    fflush(stream);
+static inline char *
+tl__log_msg_buf(void) {
+    static TL_LOG__THREAD_LOCAL char buf[TL_LOG_MSG_BUF_SIZE];
+    return buf;
 }
-#define TL__LOG_IMPL(stream, fmt, ...) \
-    tl__log_impl(stream, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
+
+static inline void
+tl__log_write(FILE *stream,
+              const char *file,
+              int line,
+              const char *func,
+              const char *msg) {
+#if !defined(TL_LOG_FAST)
+    time_t     t = time(NULL);
+    struct tm  tm_info;
+    struct tm *tm_ptr = NULL;
+#  if defined(_WIN32)
+    if (localtime_s(&tm_info, &t) == 0) {
+        tm_ptr = &tm_info;
+    }
+#  else /* POSIX */
+    if (localtime_r(&t, &tm_info) != NULL) {
+        tm_ptr = &tm_info;
+    }
 #  endif
+    char time_buf[20];
+    if (tm_ptr) {
+        strftime(time_buf, sizeof time_buf, "%Y-%m-%d %H:%M:%S", tm_ptr);
+    } else {
+        snprintf(time_buf, sizeof time_buf, "0000-00-00 00:00:00");
+    }
+    fprintf(stream, "[%s] " TL_LOG__FMT_STR "%s\n", time_buf, file, line, func, msg);
+#else /* !defined(TL_LOG_FAST) */
+    fprintf(stream, TL_LOG__FMT_STR "%s\n", file, line, func, msg);
+#endif
+}
+
+#define TL__LOG_IMPL(stream, prefix, fmt, ...)                                               \
+    do {                                                                                     \
+        char *tl__msg_buf = tl__log_msg_buf();                                               \
+        int   tl__n = snprintf(tl__msg_buf, TL_LOG_MSG_BUF_SIZE, prefix fmt, ##__VA_ARGS__); \
+        if (tl__n < 0 && TL_LOG_MSG_BUF_SIZE > 0) {                                          \
+            tl__msg_buf[0] = '\0';                                                           \
+        } else if ((size_t)tl__n >= TL_LOG_MSG_BUF_SIZE && TL_LOG_MSG_BUF_SIZE >= 4) {       \
+            tl__msg_buf[TL_LOG_MSG_BUF_SIZE - 4] = '.';                                      \
+            tl__msg_buf[TL_LOG_MSG_BUF_SIZE - 3] = '.';                                      \
+            tl__msg_buf[TL_LOG_MSG_BUF_SIZE - 2] = '.';                                      \
+            tl__msg_buf[TL_LOG_MSG_BUF_SIZE - 1] = '\0';                                     \
+        }                                                                                    \
+        tl__log_write(stream, __FILE__, __LINE__, __func__, tl__msg_buf);                    \
+    } while (0)
+
+#if TL_LOG_LEVEL <= TL_LOG_LEVEL_DEBUG
+#define LOG_DEBUG(fmt, ...) TL__LOG_IMPL(TL_LOG_STREAM, "[DEBUG] ", fmt, ##__VA_ARGS__)
+#else
+#define LOG_DEBUG(...) ((void)0)
 #endif
 
+#if TL_LOG_LEVEL <= TL_LOG_LEVEL_INFO
+#define LOG_INFO(fmt, ...) TL__LOG_IMPL(TL_LOG_STREAM, "[INFO] ", fmt, ##__VA_ARGS__)
+#else
+#define LOG_INFO(...) ((void)0)
+#endif
 
-#define LOG_DEBUG(fmt, ...) TL__LOG_IMPL(TL_LOG_STREAM, "[DEBUG] " fmt, ##__VA_ARGS__)
-#define LOG_INFO(fmt, ...)  TL__LOG_IMPL(TL_LOG_STREAM, "[INFO] " fmt, ##__VA_ARGS__)
-#define LOG_WARN(fmt, ...)  TL__LOG_IMPL(TL_LOG_STREAM, "[WARN] " fmt, ##__VA_ARGS__)
-#define LOG_ERROR(fmt, ...) TL__LOG_IMPL(TL_LOG_STREAM_ERR, "[ERROR] " fmt, ##__VA_ARGS__)
+#if TL_LOG_LEVEL <= TL_LOG_LEVEL_WARN
+#define LOG_WARN(fmt, ...) TL__LOG_IMPL(TL_LOG_STREAM, "[WARN] ", fmt, ##__VA_ARGS__)
+#else
+#define LOG_WARN(...) ((void)0)
+#endif
 
+#if TL_LOG_LEVEL <= TL_LOG_LEVEL_ERROR
+#define LOG_ERROR(fmt, ...) TL__LOG_IMPL(TL_LOG_STREAM_ERR, "[ERROR] ", fmt, ##__VA_ARGS__)
+#else
+#define LOG_ERROR(...) ((void)0)
+#endif
 
-#endif  /* TINYLIB_LOGS_H */
+#endif /* TINYLIB_LOGS_H */
